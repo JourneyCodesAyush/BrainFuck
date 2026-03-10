e1caefbc12e5292863c9ee18abb2d1613b712c10
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-03-10 18:15:54 +0530

    feat(cli): add robust argument parsing in main.cpp
    - Use single loop to parse flags and filename
    - Support --mode=char|ir flags in any order
    - Default to REPL if no file provided
    - Remove old interpreter and prompt functions
    - Wrap execution in try/catch for safe error reporting

--- engines/cpp/src/main.cpp
+++ engines/cpp/src/main.cpp
@@ -3,88 +3,54 @@
 #include <filesystem>
 #include <sstream>
 
-#include "interpreter.hpp"
-
-void print_help()
-{
-    std::cout << " ____            _       _____           _    \n"
-              << "| __ ) _ __ __ _(_)_ __ |  ___|_/\\__ ___| | __\n"
-              << "|  _ \\| '__/ _` | | '_ \\| |_  \\    // __| |/ /\n"
-              << "| |_) | | | (_| | | | | |  _| /_  _\\ (__|   < \n"
-              << "|____/|_|  \\__,_|_|_| |_|_|     \\/  \\___|_|\\_\\\n";
-
-    std::cout << "\nStart with this 'Hello World' program\n";
-    std::cout << "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>\n\n";
-}
-
-void run(interpreter::Interpreter &intrprt, const std::string &source)
-{
-    intrprt.set_source(source);
-    intrprt.interpret();
-}
-
-void run_prompt()
-{
-    print_help();
-    interpreter::Interpreter intrprt;
-    std::string source;
-    while (true)
-    {
-        std::cin.clear();
-        std::cout << "$ ";
-        if (!std::getline(std::cin, source))
-        {
-            // End-of-file (Ctrl+D / Ctrl+Z)
-            std::cout << "\nExiting BrainF*ck REPL...\n";
-            break;
-        }
-
-        if (source == "q" || source == "quit" || source == "exit")
-        {
-            std::cout << "Exiting BrainF*ck REPL...\n";
-            break;
-        }
-
-        if (!source.empty()) // only run non-empty input
-            run(intrprt, source);
-    }
-}
-
-void run_file(const char *filename)
-{
-    namespace fs = std::filesystem;
-    auto file = fs::path(filename);
-
-    interpreter::Interpreter intrprt;
-
-    if (file.extension().string() == ".bf" or file.extension().string() == ".b")
-    {
-        std::stringstream source;
-        std::ifstream source_file(filename);
-        if (!source_file.is_open())
-        {
-            std::cerr << "Could not open file " << filename << "\n!";
-            exit(EXIT_FAILURE);
-        }
-        source << source_file.rdbuf();
-        run(intrprt, source.str());
-    }
-}
+#include "char_interpreter/interpreter.hpp"
+#include "runner/brainfuck-runner.hpp"
 
 int main(int argc, char *argv[])
 {
-    if (argc == 1)
+    try
     {
-        run_prompt();
+        std::string filename;
+        runner::Mode mode = runner::Mode::CHAR;
+
+        std::string arg;
+        for (int i = 1; i < argc; i++)
+        {
+            arg = argv[i];
+            if (arg.rfind("--mode=", 0) == 0)
+            {
+                std::string value = arg.substr(7); // get the part after '='
+                if (value == "char")
+                    mode = runner::Mode::CHAR;
+                else if (value == "ir")
+                    mode = runner::Mode::IR;
+                else
+                    throw std::runtime_error("Unknown mode: " + value);
+            }
+            else if (filename.empty())
+            {
+                filename = arg;
+            }
+            else
+            {
+                throw std::runtime_error("Unknown argument: " + arg + "\n");
+            }
+        }
+
+        runner::BrainFuckRunner bfRunner(mode);
+        if (filename.empty())
+        {
+            bfRunner.runRepl();
+        }
+        else
+        {
+            bfRunner.runFromFile(filename);
+        }
     }
-    else if (argc == 2)
+    catch (const std::exception &e)
     {
-        run_file(argv[1]);
-    }
-    else
-    {
-        std::cout << "What the 'cake' did you do?\n [Insert slang as per your taste in place of 'cake']\n"
-                  << "Usage: brainfuck [filename]";
+        std::cerr << "ERROR: "
+                  << e.what() << "\n";
     }
 
     return 0;
