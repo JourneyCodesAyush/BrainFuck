f9f34016ec8d2b30e4c0a36b3671c8c8cc68b917
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-03-14 19:01:14 +0530

    fix(vm): initialize memory_pointer to 0 in constructor
    - memory_pointer attribute was initialized to 0 in one of the constructors and some other value in the overloaded constructor
    - Attribute now intiailized to 0 in all constructors

--- engines/cpp/src/ir_vm/virtual-machine/vm.hpp
+++ engines/cpp/src/ir_vm/virtual-machine/vm.hpp
@@ -21,7 +21,7 @@ namespace vm
         void takeInput();
 
     public:
-        VirtualMachine(std::istream &input_stream = std::cin) : input(input_stream), instructions({}), ip(0), memory_pointer(MAX_SIZE / 2) {}
+        VirtualMachine(std::istream &input_stream = std::cin) : input(input_stream), instructions({}), ip(0), memory_pointer(0) {}
 
         VirtualMachine(const std::vector<instruction::Instruction> &instructions, std::istream &input_stream = std::cin) : input(input_stream), instructions{instructions}, ip(0), memory_pointer(0) {}
 
