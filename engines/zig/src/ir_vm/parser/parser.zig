3c5c7fbd9dd90e3d591a87d418cdd8bb06f1b183
JourneyCodesAyush <journeycodes.ayush@gmail.com>
2026-03-18 19:07:30 +0530

    refactor(parser): pass allocator by value instead of pointer
    Pass std.mem.Allocator by value instead of pointer.

--- engines/zig/src/ir_vm/parser/parser.zig
+++ engines/zig/src/ir_vm/parser/parser.zig
@@ -9,21 +9,21 @@ pub const Parser = struct {
     array: std.ArrayList(instruction.Instructions),
     loop_stack: std.ArrayList(usize),
 
-    allocator: *std.mem.Allocator,
+    allocator: std.mem.Allocator,
 
-    pub fn init(self: *Parser, source: []const u8, allocator: *std.mem.Allocator) !void {
+    pub fn init(self: *Parser, source: []const u8, allocator: std.mem.Allocator) !void {
         // std.debug.print("Initializing the parser...\n", .{});
         self.source = source;
         self.allocator = allocator;
         self.current = 0;
-        self.array = try std.ArrayList(instruction.Instructions).initCapacity(self.allocator.*, 1024);
-        self.loop_stack = try std.ArrayList(usize).initCapacity(self.allocator.*, 256);
+        self.array = try std.ArrayList(instruction.Instructions).initCapacity(self.allocator, 1024);
+        self.loop_stack = try std.ArrayList(usize).initCapacity(self.allocator, 256);
     }
 
     pub fn deinit(self: *Parser) void {
         // std.debug.print("Deinitializing the parser...\n", .{});
-        self.array.deinit(self.allocator.*);
-        self.loop_stack.deinit(self.allocator.*);
+        self.array.deinit(self.allocator);
+        self.loop_stack.deinit(self.allocator);
     }
 
     pub fn setSource(self: *Parser, source: []const u8) void {
@@ -36,31 +36,31 @@ pub const Parser = struct {
         while (self.current < self.source.len) {
             switch (self.source[self.current]) {
                 '+' => {
-                    self.array.append(self.allocator.*, emitByte(instruction.OpCode.ADD, 1)) catch return errors.Errors.ParseError;
+                    self.array.append(self.allocator, emitByte(instruction.OpCode.ADD, 1)) catch return errors.Errors.ParseError;
                 },
                 '-' => {
-                    self.array.append(self.allocator.*, emitByte(instruction.OpCode.ADD, -1)) catch return errors.Errors.ParseError;
+                    self.array.append(self.allocator, emitByte(instruction.OpCode.ADD, -1)) catch return errors.Errors.ParseError;
                 },
                 '>' => {
-                    self.array.append(self.allocator.*, emitByte(instruction.OpCode.MOVE, 1)) catch return errors.Errors.ParseError;
+                    self.array.append(self.allocator, emitByte(instruction.OpCode.MOVE, 1)) catch return errors.Errors.ParseError;
                 },
                 '<' => {
-                    self.array.append(self.allocator.*, emitByte(instruction.OpCode.MOVE, -1)) catch return errors.Errors.ParseError;
+                    self.array.append(self.allocator, emitByte(instruction.OpCode.MOVE, -1)) catch return errors.Errors.ParseError;
                 },
                 '.' => {
-                    self.array.append(self.allocator.*, emitByte(instruction.OpCode.OUTPUT, 0)) catch return errors.Errors.ParseError;
+                    self.array.append(self.allocator, emitByte(instruction.OpCode.OUTPUT, 0)) catch return errors.Errors.ParseError;
                 },
                 ',' => {
-                    self.array.append(self.allocator.*, emitByte(instruction.OpCode.INPUT, 0)) catch return errors.Errors.ParseError;
+                    self.array.append(self.allocator, emitByte(instruction.OpCode.INPUT, 0)) catch return errors.Errors.ParseError;
                 },
                 '[' => {
-                    self.loop_stack.append(self.allocator.*, self.array.items.len) catch return errors.Errors.ParseError;
-                    self.array.append(self.allocator.*, emitByte(instruction.OpCode.JZ, 0)) catch return errors.Errors.ParseError;
+                    self.loop_stack.append(self.allocator, self.array.items.len) catch return errors.Errors.ParseError;
+                    self.array.append(self.allocator, emitByte(instruction.OpCode.JZ, 0)) catch return errors.Errors.ParseError;
                 },
                 ']' => {
                     const start = self.loop_stack.pop() orelse return errors.Errors.ParseError;
                     self.array.items[start].argument = @intCast(self.array.items.len);
-                    self.array.append(self.allocator.*, emitByte(instruction.OpCode.JNZ, @intCast(start))) catch return errors.Errors.ParseError;
+                    self.array.append(self.allocator, emitByte(instruction.OpCode.JNZ, @intCast(start))) catch return errors.Errors.ParseError;
                 },
                 else => {},
             }
